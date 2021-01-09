1e84e836983427da182cdfff94ca2501d2dddb02
SesisnandoLRNeto <sesisnando.neto@apoemafabsoft.com>
2021-01-08 21:02:35 -0400

    GET /product

---
+++ dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dsdeliver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	//Query methods spring io Documentation jpa
+	List<Product> findAllByOrderByNameAsc();
+
+}
---
+++ dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll(){
+		List<Product> listProduct = productRepository.findAllByOrderByNameAsc();
+		//converter a list de products em dto e reconvertendo com stream
+		return listProduct.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+}
