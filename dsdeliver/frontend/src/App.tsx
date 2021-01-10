cd0e69b27188e954d018fdc608c41d1ebd830882
SesisnandoLRNeto <sesisnando.neto@apoemafabsoft.com>
2021-01-10 11:26:52 -0400

    Finish frontend

--- dsdeliver/frontend/src/App.tsx
+++ dsdeliver/frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import Counter from './Counter';
 import Hello from './Hello';
-
 import NavBar from './Navbar';
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 import Routes from './Routes';
 
 import './App.css';
@@ -9,7 +12,10 @@ import './App.css';
 function App() {
 
   return (
+    <>
       <Routes/>
+      <ToastContainer/>
+    </>
   );
 }
 
